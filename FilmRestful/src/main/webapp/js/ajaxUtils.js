11ab4c9c8451e4fc033f1f33d0079d73fccd4d00
Jordan-Prescott <Jprescott23@gmail.com>
2023-04-17 19:44:08 +0100

    jquery loads db on page load

--- FilmRestful/src/main/webapp/js/ajaxUtils.js
+++ FilmRestful/src/main/webapp/js/ajaxUtils.js
@@ -1,17 +1,46 @@
 /**
  * 
  */
-function loadPage() {
-  $.ajax({
-  url: "http://localhost:8080/FilmRestful/FilmsAPI",
-  dataType: "json",
-  success: function(data) {
-    // Process the data that is returned
-    console.log(data);
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    // Handle any errors that occur during the request
-    console.error("Error: " + textStatus, errorThrown);
-  }
+
+$(function() {
+	$("#updateFilm").click(updateFilm);
+	$("#deleteFilm").click(deleteFilm);
+	$("#filmDB").click(loadPage);
 });
+
+function loadPage() {
+	$.ajax({
+		url: "FilmsAPI",
+		type: "GET",
+		dataType: "json",
+		success: function(data) {
+			var table = $('#filmTable');
+
+			$.each(data, function(i, film) {
+				var tableRow = $('<tr>');
+				tableRow.append($('<td>').text(film.title));
+				tableRow.append($('<td>').text(film.year));
+				tableRow.append($('<td>').text(film.director));
+				tableRow.append($('<td>').text(film.stars));
+				tableRow.append($('<td>').text(film.genre));
+				tableRow.append($('<td>').text(film.rating));
+				tableRow.append($('<td>').text(film.review));
+				tableRow.append($('<td type="button" class="btn btn-link" id="updateFilm">Update</td>'));
+				tableRow.append($('<td type="button" class="btn btn-link" id="deleteFilm">Delete</td>'));
+				table.append(tableRow);
+			})
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			// Handle any errors that occur during the request
+			console.error("Error: " + textStatus, errorThrown);
+		},
+		complete: function() {
+			// Request complete
+			console.log("Load page complete");
+		}
+	});
+}
+
+function updateFilm() {
+	console.log("get the fuck out of here.")
 }
