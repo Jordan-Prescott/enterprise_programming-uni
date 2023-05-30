09b4d58a5f78232d4ac8aa2057542e7a7dc88ee7
Jordan Prescott <66872617+Jordan-Prescott@users.noreply.github.com>
2023-05-30 19:27:17 +0100

    Delete ajax-basics directory

--- ajax-basics/src/coreservlets/TimeUtils.java
+++
@@ -1,9 +0,0 @@
-package coreservlets;
-
-public class TimeUtils {
-  public static void pause(double seconds) {
-    try {
-      Thread.sleep((long)(seconds*1000));
-    } catch(InterruptedException e) {}
-  }
-}
