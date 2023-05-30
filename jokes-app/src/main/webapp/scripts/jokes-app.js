f0c316f3bff19b90a75765447bb7a51919feeca0
Jordan Prescott <66872617+Jordan-Prescott@users.noreply.github.com>
2023-05-30 19:28:39 +0100

    Delete jokes-app directory

--- jokes-app/src/main/webapp/scripts/jokes-app.js
+++
@@ -1,9 +0,0 @@
-function getJoke(rr){
-	
-	ajaxResult("https://official-joke-api.appspot.com/jokes/programming/random", rr);
-}
-
-
-document.addEventListener("DOMContentLoaded", function(){
-    getJoke('joke');
-});
